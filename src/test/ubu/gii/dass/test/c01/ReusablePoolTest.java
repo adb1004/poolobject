ab7498ea759653ab7b3e541aa024655097495440
ivan <ica1006@alu.ubu.es>
2022-02-20 20:22:22 +0100

    ReusablePoolTest.java done

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -1,25 +1,38 @@
 /**
  * 
  */
-package ubu.gii.dass.test.c01;
+package test.ubu.gii.dass.test.c01;
 
 import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import main.ubu.gii.dass.c01.DuplicatedInstanceException;
+import main.ubu.gii.dass.c01.NotFreeInstanceException;
+import main.ubu.gii.dass.c01.Reusable;
+import main.ubu.gii.dass.c01.ReusablePool;
 
 /**
- * @author alumno
+ * @author Iván Cortés Aliende
+ * @author Alberto Diez Busto
  *
  */
 public class ReusablePoolTest {
+	
+	private ReusablePool pool;
+	private int testPasados = 0;
+	
+	
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		pool = ReusablePool.getInstance();
 	}
 
 	/**
@@ -27,6 +40,16 @@ public class ReusablePoolTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		
+		if (testPasados == 3) {
+			while(true) {
+				try {
+					pool.acquireReusable();
+				}catch (NotFreeInstanceException exception) {
+					break;
+				}
+			}
+		}
 	}
 
 	/**
@@ -34,7 +57,10 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testGetInstance() {
-		fail("Not yet implemented");
+		ReusablePool pool2 = ReusablePool.getInstance();
+		
+		assertEquals(pool,pool2);
+		testPasados++;
 	}
 
 	/**
@@ -42,7 +68,23 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testAcquireReusable() {
-		fail("Not yet implemented");
+		
+		Reusable objeto;
+		int contador = 0;
+				
+		while(true) {
+			try {
+				objeto = pool.acquireReusable();
+				if (objeto != null) {
+					contador++;
+				}
+			}catch(NotFreeInstanceException exception) {
+				System.out.println(contador);
+				assertTrue(contador == 2);
+				break;
+			}
+		}
+		testPasados++;
 	}
 
 	/**
@@ -50,7 +92,33 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testReleaseReusable() {
-		fail("Not yet implemented");
+		Reusable objeto = new Reusable();
+		Boolean exito = false;
+		
+		try{
+			// Agregamos el objeto. No debería encontrarse en el pool.
+			pool.releaseReusable(objeto);
+			exito = true;
+		}catch (DuplicatedInstanceException exception) {
+			fail("Excepcion no esperada, ya existe el objeto");
+		}
+		
+		try{
+			// Agregamos el objeto otra vez. Ahora si que deberia encontrarse, por lo que deberia saltar
+			// la excepcion
+			pool.releaseReusable(objeto);
+		}catch (DuplicatedInstanceException exception) {
+			assertTrue(true);
+			
+		}
+		
+		// Retiramos el objeto introducido
+		if (exito == true) {
+			try{
+				pool.acquireReusable();
+			}catch (NotFreeInstanceException exception2) {}
+		}
+		testPasados++;	
 	}
 
 }
