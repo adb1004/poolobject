1dd8f9d4ac4c01f117b9ea106040e33b6e5b0104
adb1004 <74242270+adb1004@users.noreply.github.com>
2022-02-24 20:15:27 +0100

    Testing ClientTest 2

--- src/test/ubu/gii/dass/test/c01/ClientTest.java
+++ src/test/ubu/gii/dass/test/c01/ClientTest.java
@@ -1,18 +1,2 @@
-package test.ubu.gii.dass.test.c01;
 
-import org.junit.Test;
-
-import main.ubu.gii.dass.c01.Client;
-
-/**
- * @author Iván Cortés Aliende
- * @author Alberto Diez Busto
- *
- */
-public class ClientTest {
-	/**
-	 * Test que comprueba que se creen correctamente los objetos de la clase Client
-	 */
-
-}
 
